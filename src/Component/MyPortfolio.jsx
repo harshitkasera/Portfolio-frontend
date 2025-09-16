98186aec82de11a514ef35d991d3c01ee0ae3b03
Harshit Kasera <harshitkasera01@gmail.com>
2025-09-16 09:24:16 +0530

    first

--- src/Component/MyPortfolio.jsx
+++ src/Component/MyPortfolio.jsx
@@ -35,7 +35,7 @@ const MyPortfolio = () => {
           <h1 className='full-h1'>Hi, I'm Harshit Kasera</h1>
         </div>
         <div className='prof'>
-          <h4>Creative Designer & Developer</h4>
+          <h4>MERN STACK DEVELOPER</h4>
         </div>
         <div className='fullbtn'>
           <a href='#4'><button className='dpbtn'>View My Work</button></a>
@@ -43,7 +43,7 @@ const MyPortfolio = () => {
       </div>
 
       {/* About Section */}
-      <div  id="2" className='parallax_Section'>
+      <div id="2" className='parallax_Section'>
         <div className='paralex-bg' style={about}></div>
         <h2 className='abou-h'>About Me</h2>
         <div className='abo-box'>
@@ -52,10 +52,10 @@ const MyPortfolio = () => {
           </div>
 
           <div >
-            
-            <p  className='about-text'>My name is Harshit Kasera, currently I live in Indore and I belong to Shajapur.
+
+            <p className='about-text'>My name is Harshit Kasera, currently I live in Indore and I belong to Shajapur.
               Currently I'm doing an Internship at Indvibe Infotech Pvt Ltd.
-              Recently I have completed my B-Tech in Computer Science and Engineering 
+              Recently I have completed my B-Tech in Computer Science and Engineering
               from Mathuradevi Institute of Technology, and my class 10th is percentage 72.9 from Sandeepani Gyanpeeth School Shajapur,and my class 12th is percentage 72.2 from We Care Higher Secondary School Shajapur, I have gained valuable skill HTML CSS JavaScript and React, and the other backend is MongoDB, Express.js, Node.js, and I'm talking about my goal, So short term goal is to become FullStack Developer, and Long term goal is to work reputed companies,
               and My Strength is quick learner and passionate about frontend technologies, and my hobby  is trading and market Analysis, so thats all about me Thankyou
             </p>
@@ -65,25 +65,25 @@ const MyPortfolio = () => {
       </div>
 
       {/* Skills Section */}
-      <div   id='3' >
-      <div  className='parallax_Section'>
-        {/* <div className='paralex-bgt'></div> */}
-        <div className='box'>
-        <h2 className='sk'>Skills</h2>
-        <div className="skills-container">
-      <a className='x' href='https://www.geeksforgeeks.org/mongodb/what-is-mongodb-working-and-features/'>
-          <div className='s-img'><img src='./Images/mongo.png' className='img' alt="mongo"/>MongoDB</div>
-        </a>
-      <a className='x' href='https://expressjs.com/'>
-          <div className='s-img'><img src='./Images/express.png' className='img' alt="express"/>Express</div></a>
-      <a className='x' href='https://react.dev/'>
-          <div className='s-img'><img src='./Images/react.png' className='img' alt="react"/>React</div></a>
-      <a className='x' href='https://nodejs.org/en/learn/getting-started/introduction-to-nodejs'>
-          <div className='s-img'><img src='./Images/node.png' className='img' alt="node"/>Node.js</div></a>
+      <div id='3' >
+        <div className='parallax_Section'>
+          {/* <div className='paralex-bgt'></div> */}
+          <div className='box'>
+            <h2 className='sk'>Skills</h2>
+            <div className="skills-container">
+              <a className='x' href='https://www.geeksforgeeks.org/mongodb/what-is-mongodb-working-and-features/'>
+                <div className='s-img'><img src='./Images/mongo.png' className='img' alt="mongo" />MongoDB</div>
+              </a>
+              <a className='x' href='https://expressjs.com/'>
+                <div className='s-img'><img src='./Images/express.png' className='img' alt="express" />Express</div></a>
+              <a className='x' href='https://react.dev/'>
+                <div className='s-img'><img src='./Images/react.png' className='img' alt="react" />React</div></a>
+              <a className='x' href='https://nodejs.org/en/learn/getting-started/introduction-to-nodejs'>
+                <div className='s-img'><img src='./Images/node.png' className='img' alt="node" />Node.js</div></a>
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-</div>
       {/* Projects Section */}
       <div id='4' className='parallax_Section'>
         {/* <div className='paralex-bgf'></div> */}
@@ -97,7 +97,7 @@ const MyPortfolio = () => {
               <p>{items.desc}</p>
               <a href='https://employeeprojec.netlify.app/'><button className='buton'>Check Project</button></a>
             </div>
-            
+
           ))}
         </div>
       </div>
@@ -107,15 +107,15 @@ const MyPortfolio = () => {
         <h2 id='5'>Contact</h2>
         <p>+91-9302252353</p>
         <div className='full-fot'>
-       <div className='icon-fot'>  <FaLinkedinIn className='icon'/>
-         <a className='fot' href='https://www.linkedin.com/in/harshit-kasera-75b220263/'>Harshit Kasera</a></div>
-       <div className='icon-fot'>  <FaInstagram className='icon'/>
-         <a className='fot' href='https://www.instagram.com/'>kasera_353_</a>
-        <div className='icon-fot'>
-        <i className="fa-solid fa-envelope"></i>
-         <a className='fot' href='https://mail.google.com/mail/u/0/#inbox'>harshitkasera01@gmail.com</a></div>
-         </div>
-</div>
+          <div className='icon-fot'>  <FaLinkedinIn className='icon' />
+            <a className='fot' href='https://www.linkedin.com/in/harshit-kasera-75b220263/'>Harshit Kasera</a></div>
+          <div className='icon-fot'>  <FaInstagram className='icon' />
+            <a className='fot' href='https://www.instagram.com/'>kasera_353_</a>
+            <div className='icon-fot'>
+              <i className="fa-solid fa-envelope"></i>
+              <a className='fot' href='https://mail.google.com/mail/u/0/#inbox'>harshitkasera01@gmail.com</a></div>
+          </div>
+        </div>
 
       </footer>
     </div>
